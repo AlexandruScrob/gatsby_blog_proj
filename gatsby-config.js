ad68f04d84298d90c8d12642ec7e729a9b9e3817
Alexandru <alexader_alexandruscrob@yahoo.com>
2022-09-07 23:55:08 +0300

    Finished blog proj

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,11 +10,20 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         name: `markdowns`,
         path: `${__dirname}/src/markdown-pages`,
       },
     },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
